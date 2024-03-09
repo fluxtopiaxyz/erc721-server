c1b13934de833deb2f9c0837fc04ed2c359a2d41
Your Name <you@example.com>
2024-03-09 09:38:03 +0700

    add module

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,9 @@
+const random_name = require('node-random-name');
+
 export default function handler(req, res) {
-  const { name } = req.query;
+  const query = req.query;
   return res.json({
-    message: `Hello ${name}!`,
+    message: 'Hello ' + random_name(),
+    query
   });
 }
